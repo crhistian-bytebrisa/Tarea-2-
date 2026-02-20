42cdae341c30c1913dfdc5d1e24a3fcc2be131f2
J.Crhistian <j.crhistiantr@gmail.com>
2026-02-20 06:59:56 -0400

    Guiate de eso en el Back, el front no esta hecho

---
+++ Backend/Peliculas.Application/Interfaces/IGeneroService.cs
@@ -0,0 +1,17 @@
+﻿using Peliculas.Application.DTO;
+using Peliculas.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peliculas.Application.Interfaces
+{
+    public interface IGeneroService
+    {
+        public Task<GeneroDTO> GetByIdAsync();
+        public Task<List<GeneroSimpleDTO>> GetAllAsync();
+        public Task<GeneroDTO> AddAsync(GeneroCreateDTO dto);
+        public Task<GeneroDTO> UpdateAsync(GeneroUpdateDTO dto, int Id);
+        public Task DeleteByIdAsync(int Id);
+    }
+}
---
+++ Backend/Peliculas.Application/Services/PeliculaService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peliculas.Application.Services
+{
+    internal class PeliculaService
+    {
+    }
+}
